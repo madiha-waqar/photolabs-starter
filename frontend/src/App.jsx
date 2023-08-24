a45af729b38884cc897d9d28697a2aa22f5960a5
madiha-waqar <mail2madihawaqar@gmail.com>
2023-08-24 01:17:43 -0400

    Create top navigation bar with topics and heart icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PhotoList from "./components/PhotoList";
 import "./App.scss";
 import TopicList from "components/TopicList";
+import TopNavigationBar from "./components/TopNavigationBar";
 
 const App = () => {
   return (
     <div className="App">
+      <TopNavigationBar />
       <PhotoList />
       <TopicList />
     </div>
